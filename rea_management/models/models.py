8ea0d14618d4b7e23699c7ba68c8e7b1f7afe1d9
enginov-z <63125136+enginov-z@users.noreply.github.com>
2020-04-04 21:16:01 +0100

    Update models.py

--- rea_management/models/models.py
+++ rea_management/models/models.py
@@ -57,18 +57,7 @@ class ProductTemplateInherit(models.Model):
             
         }
 
-class ResPartnerInherit(models.Model):
-    _inherit="res.partner"
 
-    birthday = fields.Date('Date de naissance')
-    age = fields.Integer('Age')
-
-    @api.depends('birthday')
-    def _set_age(self):
-        age = int((datetime.datetime.today() - self.birthday) / 365)
-        self.update({
-            'age': age
-        })
 
 
 
