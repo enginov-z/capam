9cccdfa061999eda9e1fcf156b5bdf30c752660f
enginov-z <63125136+enginov-z@users.noreply.github.com>
2020-04-04 23:30:20 +0100

    Update models.py

--- rea_management/models/models.py
+++ rea_management/models/models.py
@@ -60,7 +60,10 @@ class ProductTemplateInherit(models.Model):
 class ResPartnerInherit(models.Model):
     _inherit="res.partner"
 
-    x_studio_current_company_2 = fields.Char('bla bla', default="bla bla")
+    x_studio_current_company_2 = fields.Many2one('res.partner', compute="get_default_company", store=True)
+
+    def get_default_company(self):
+        return self.env.company_id.id
 
     @api.onchange('x_studio_date_de_naissance')
     def set_age(self):
