5c4ae8389d5408752824b84431078f2ea4741486
enginov-z <63125136+enginov-z@users.noreply.github.com>
2020-04-05 00:56:26 +0100

    Update models.py

--- rea_management/models/models.py
+++ rea_management/models/models.py
@@ -62,7 +62,7 @@ class ResPartnerInherit(models.Model):
 
     def get_default_company(self):
         company_id = self.env['res.company'].search([('name','=',self.company_name)]).id
-        self.x_studio_current_company_2 = company_id
+        self.x_studio_current_company_2 = company_id.id
         if self.company_name == self.x_studio_current_company_2.name:
             self.update({
                 'x_studio_current_company_bool' : True
