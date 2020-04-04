5bedc91d6e640831fa4d94bb7f6d6563c4292e9c
enginov-z <63125136+enginov-z@users.noreply.github.com>
2020-04-04 15:48:57 +0100

    add two attributes to product template

--- rea_management/models/models.py
+++ rea_management/models/models.py
@@ -13,3 +13,12 @@ class SaleOrderInherit(models.Model):
         ('returned', 'Libre'),
         ('cancel', 'Annulé'),
     ], string="Etat des affectations", compute='_compute_rental_status', store=True)
+
+class ProductTemplateInherit(models.Model):
+    _inherit="product.template"
+
+    today_state = fields.Char(compute="_get_daily_state", string="Etat d'aujourd'hui")
+    next_free_dtae = fields.Date('Date de Disponibilité')
+
+    def _get_daily_state(self):
+        return "Libre"
