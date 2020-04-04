e62d320256b40f4138673624b0cfbaffee25abbf
enginov-z <63125136+enginov-z@users.noreply.github.com>
2020-04-04 02:10:21 +0100

    ed

--- rea_management/controllers/controllers.py
+++ rea_management/controllers/controllers.py
@@ -1,20 +1,20 @@
 # -*- coding: utf-8 -*-
 from odoo import http
 
-# class ReaManagement(http.Controller):
-#     @http.route('/rea_management/rea_management/', auth='public')
+# class MyModule(http.Controller):
+#     @http.route('/my_module/my_module/', auth='public')
 #     def index(self, **kw):
 #         return "Hello, world"
 
-#     @http.route('/rea_management/rea_management/objects/', auth='public')
+#     @http.route('/my_module/my_module/objects/', auth='public')
 #     def list(self, **kw):
-#         return http.request.render('rea_management.listing', {
-#             'root': '/rea_management/rea_management',
-#             'objects': http.request.env['rea_management.rea_management'].search([]),
+#         return http.request.render('my_module.listing', {
+#             'root': '/my_module/my_module',
+#             'objects': http.request.env['my_module.my_module'].search([]),
 #         })
 
-#     @http.route('/rea_management/rea_management/objects/<model("rea_management.rea_management"):obj>/', auth='public')
+#     @http.route('/my_module/my_module/objects/<model("my_module.my_module"):obj>/', auth='public')
 #     def object(self, obj, **kw):
-#         return http.request.render('rea_management.object', {
+#         return http.request.render('my_module.object', {
 #             'object': obj
 #         })
