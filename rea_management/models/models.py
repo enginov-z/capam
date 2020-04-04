5b642cb99e9bc21e1a2c8558e3ddf2dacef07522
enginov-z <63125136+enginov-z@users.noreply.github.com>
2020-04-04 23:58:25 +0100

    Update models.py

--- rea_management/models/models.py
+++ rea_management/models/models.py
@@ -62,7 +62,9 @@ class ResPartnerInherit(models.Model):
 
     def get_default_company(self):
         self.x_studio_current_company_2 = self.env.company.id
-        
+        self.update({
+            'x_studio_current_company_temp' : self.env.company.id
+        })
     x_studio_current_company_2 = fields.Many2one('res.company', compute=get_default_company)
 
     
