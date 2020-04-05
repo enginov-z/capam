12d0e2d5dd02c818d1f71dab1a3012fb6592f701
enginov-z <63125136+enginov-z@users.noreply.github.com>
2020-04-05 18:59:21 +0100

    Update map_render.js

--- rea_management/static/src/js/map_render.js
+++ rea_management/static/src/js/map_render.js
@@ -1,8 +1,53 @@
 odoo.define('map_render_custom',function(require){
 var map_renderer_original = require('web_map.MapRenderer');
 
-map_renderer_original._addMakers.include(function(){
-    console.log('haha i worked')
+map_renderer_original.include({
+    _addMakers: function (records) {
+        var self = this;
+        this._removeMakers();
+        records.forEach(function (record) {
+            if (record.partner && record.partner.partner_latitude && record.partner.partner_longitude) {
+                var popup = {};
+                popup.records = self._getMarkerPopupFields(record, self.fieldsMarkerPopup);
+                popup.url = 'https://www.google.com/maps/dir/?api=1&destination=' + record.partner.partner_latitude + ',' + record.partner.partner_longitude;
+                var $popup = $(qweb.render('map-popup', { records: popup }));
+                var openButton = $popup.find('button.btn.btn-primary.edit')[0];
+                if (self.hasFormView) {
+                    openButton.onclick = function () {
+                        self.trigger_up('open_clicked',
+                            { id: record.id });
+                    };
+                } else {
+                    openButton.remove();
+                }
+
+                var marker;
+                var offset;
+                if (self.numbering) {
+                    var number = L.divIcon({
+                        className: 'o_numbered_marker',
+                        html: '<p class ="o_number_icon">' + (self.state.records.indexOf(record) + 1) + '</p>'
+                    });
+                    marker = L.marker([record.partner.partner_latitude, record.partner.partner_longitude], { icon: number });
+                    offset = new L.Point(0, -35);
+
+                } else {
+                    marker = L.marker([record.partner.partner_latitude, record.partner.partner_longitude]);
+                    offset = new L.Point(0, 0);
+                }
+                marker
+                    .addTo(self.leafletMap)
+                    .bindPopup(function () {
+                        var divPopup = document.createElement('div');
+                        $popup.each(function (i, element) {
+                            divPopup.appendChild(element);
+                        });
+                        return divPopup;
+                    }, { offset: offset });
+                self.markers.push(marker);
+            }
+        });
+    },
 })
 
 });
