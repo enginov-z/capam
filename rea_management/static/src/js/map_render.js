131110d319d3a68e08bffd1f933a68575913ad2e
enginov-z <63125136+enginov-z@users.noreply.github.com>
2020-04-05 20:12:54 +0100

    map cust

--- rea_management/static/src/js/map_render.js
+++ rea_management/static/src/js/map_render.js
@@ -28,10 +28,12 @@ map_renderer_original.include({
 
                 var marker;
                 var offset;
+
                 if (self.numbering) {
                     var number = L.divIcon({
                         className: 'o_numbered_marker',
-                        html: '<p class ="o_number_icon">' + (self.state.records.indexOf(record) + 1) + '</p>'
+                        html: '<p class ="o_number_icon">' + (self.state.records.indexOf(record) + 1) + '</p>',
+                        iconUrl: '/rea_management/static/src/img/marker-blue.png',
                     });
                     marker = L.marker([record.partner.partner_latitude, record.partner.partner_longitude], { icon: number });
                     offset = new L.Point(0, -35);
@@ -39,7 +41,8 @@ map_renderer_original.include({
                 } else {
                     var number = L.divIcon({
                         className: 'o_numbered_marker',
-                        html: '<p class ="o_number_icon">' + (record.x_studio_lits_disponible ) + '</p>'
+                        html: '<p class ="o_number_icon">' + (record.x_studio_lits_disponible ) + '</p>',
+                        iconUrl: '/rea_management/static/src/img/marker-blue.png',
                     });
                     marker = L.marker([record.partner.partner_latitude, record.partner.partner_longitude], { icon: number });
                     offset = new L.Point(0, -35);
