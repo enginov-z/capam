de7da849366eb29a5319d07c0a001162c75d6fd7
enginov-z <63125136+enginov-z@users.noreply.github.com>
2020-04-05 18:56:22 +0100

    Update map_render.js

--- rea_management/static/src/js/map_render.js
+++ rea_management/static/src/js/map_render.js
@@ -1,4 +1,4 @@
-onmousedown.define('map_render_custom',function(require){
+odoo.define('map_render_custom',function(require){
 var map_renderer_original = require('web_map.MapRenderer');
 
 map_renderer_original._addMakers.include(function(){
