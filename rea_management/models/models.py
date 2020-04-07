e07f34bd23a114515a9b2758f7e04ebd33186868
enginov-z <63125136+enginov-z@users.noreply.github.com>
2020-04-07 16:35:24 +0100

    Update models.py

--- rea_management/models/models.py
+++ rea_management/models/models.py
@@ -95,7 +95,8 @@ class ResCompanyInherit(models.Model):
 
     def get_full_address(self):
         a = "{0},{1},{2},{3}".format(self.street,self.zip, self.city , self.country_id.name)
-        self.x_studio_contact_address_complete = a
+        self.update({
+            'x_studio_contact_address_complete' = a
 
     x_studio_contact_address_complete = fields.Char('Contact adress complete', compute=get_full_address)
 
