a3f0e04fe11cc58e4dd27a585b261b65a6aa9e73
enginov-z <63125136+enginov-z@users.noreply.github.com>
2020-04-07 16:27:01 +0100

    Update models.py

--- rea_management/models/models.py
+++ rea_management/models/models.py
@@ -126,8 +126,8 @@ class ResCompanyInherit(models.Model):
             'x_studio_lit_totals' : len(self.x_studio_field_keWp2_1)
         })
         
-    x_studio_available_beds_temp_1_1 = fields.Integer('Lits Disponible')
-    x_studio_total_beds_temp_1 = fields.Integer('Lits totale')
+    x_studio_available_beds_temp_1_1 = fields.Integer('Lits Disponible', compute=get_available_beds)
+    x_studio_total_beds_temp_1 = fields.Integer('Lits totale',, compute=get_total_beds)
 
 class ReportRangeAvailability(models.Model):
     _name = "report.range.availability1"
