8139f1c7012347b45103027fada7f8bacc96caaa
enginov-z <63125136+enginov-z@users.noreply.github.com>
2020-04-08 16:53:11 +0100

    Update map_controller.js

--- rea_management/static/src/js/map_controller.js
+++ rea_management/static/src/js/map_controller.js
@@ -1,5 +1,6 @@
 odoo.define('map_controller_custom',function(require){
     var core = require('web.core');
+    var AbstractController = require('web.AbstractController');
     var AbstractRenderer = require('web.AbstractRenderer');
     var field_utils = require('web.field_utils');
     var qweb = core.qweb;
